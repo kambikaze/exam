8719dcdf2101122dbfc2c0a5786880fab64d9cd2
kambikaze <115198918+kambikaze@users.noreply.github.com>
2022-10-07 08:27:30 +0530

    Create stud.java

---
+++ stud.java
@@ -0,0 +1,21 @@
+class Student {
+    String name;
+    int rollNo;
+    int mark1;
+    int mark2;
+    int mark3;
+    Student(String n, int rno, int m1, int m2, int m3) {
+        name = n;
+        rollNo = rno;
+        mark1 = m1;
+        mark2 = m2;
+        mark3 = m3;
+    }
+    float avgMark() {
+        return (mark1 + mark2 + mark3) / 3;
+    }
+    void display() {
+        System.out.println("Name : " + name + "\nRollno : " +
+            rollNo + "\nAverage Mark : " + avgMark());
+    }
+}
