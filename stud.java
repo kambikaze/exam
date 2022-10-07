6ee534e0d4dff9d525fc25ef6f42358b85e6fd18
kambikaze <115198918+kambikaze@users.noreply.github.com>
2022-10-07 08:58:45 +0530

    Update stud.java

--- stud.java
+++ stud.java
@@ -4,6 +4,7 @@ class Student {
     int mark1;
     int mark2;
     int mark3;
+
     Student(String n, int rno, int m1, int m2, int m3) {
         name = n;
         rollNo = rno;
@@ -11,11 +12,20 @@ class Student {
         mark2 = m2;
         mark3 = m3;
     }
+
     float avgMark() {
         return (mark1 + mark2 + mark3) / 3;
     }
+
     void display() {
-        System.out.println("Name : " + name + "\nRollno : " +
-            rollNo + "\nAverage Mark : " + avgMark());
+        System.out.println("Name : " + name + "\nRollno : "
+                + rollNo + "\nAverage Mark : " + avgMark());
+    }
+}
+
+class Main {
+    public static void main(String args[]) {
+        Student s = new Student("user", 12, 34, 56, 45);
+        s.display();
     }
 }
